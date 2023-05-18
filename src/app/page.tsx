7f6719356604d68e3c85e4d1b9e67d9f5570889c
Yash Singh <saiansh2525@gmail.com>
2023-05-17 19:19:18 -0700

    feat: metadata and forms

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,16 @@ import {
   QueryClient,
   useQuery,
 } from "@tanstack/react-query";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Button } from "@/components/ui/button";
+import { Loader2 } from "lucide-react";
 
 const queryClient = new QueryClient();
 
@@ -26,19 +36,22 @@ declare global {
 
 let recognition: SpeechRecognition | undefined;
 
+const textareaClassName =
+  "flex h-20 w-full rounded-md mt-2 border border-input bg-transparent px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50";
+
 function Home() {
   const [listView, setListView] = useState(false);
-  const [running, setRunning] = useState(false);
+  const [running, setRunning] = useState<false | string>(false);
   const [animationParent] = useAutoAnimate();
   const textareaRef = useRef<HTMLTextAreaElement | null>(null);
   const [value, setValue] = useState("");
 
-  const { data, refetch } = useQuery({
+  const { data, refetch, isFetching } = useQuery({
     queryKey: ["symptoms", value],
     queryFn: async ({ queryKey }) => {
       setListView(true);
       const json = await fetch(
-        `https://symptoai.vercel.app/query?sentence=${queryKey[1]}`
+        `https://symptoai.vercel.app/query?user_info=${queryKey[1]}`
       ).then((response) => response.json());
       const diseaseOutput = [];
       for (const disease in json["matches"]) {
@@ -79,15 +92,52 @@ function Home() {
 
     if (!recognition) return;
 
-    recognition.onresult = (event) => {
-      textareaRef.current!.value +=
-        event.results[event.resultIndex][0].transcript;
-    };
-
     return () => {
       recognition!.onresult = () => {};
     };
   });
+
+  function Microphone({ id }: { id: string }) {
+    return (
+      <div className="absolute bottom-3 right-3 flex flex-row items-center">
+        {recognition ? (
+          running === id ? (
+            <div
+              className="w-3 h-3 cursor-pointer bg-red-900 p-0 record-pulse box-content border-0"
+              onClick={() => {
+                recognition!.stop();
+                setRunning(false);
+              }}
+            ></div>
+          ) : (
+            <div
+              className={`w-3 ${
+                running ? "cursor-not-allowed" : "cursor-pointer"
+              }`}
+              aria-disabled={!!running}
+              onClick={() => {
+                if (running) {
+                  return;
+                }
+                recognition!.onresult = (event) => {
+                  (document.getElementById(id) as HTMLTextAreaElement).value +=
+                    event.results[event.resultIndex][0].transcript;
+                };
+                recognition!.start();
+                setRunning(id);
+              }}
+              dangerouslySetInnerHTML={{
+                __html: `<svg xmlns="http://www.w3.org/2000/svg" fill="white" viewBox="0 0 384 512"><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M192 0C139 0 96 43 96 96V256c0 53 43 96 96 96s96-43 96-96V96c0-53-43-96-96-96zM64 216c0-13.3-10.7-24-24-24s-24 10.7-24 24v40c0 89.1 66.2 162.7 152 174.4V464H120c-13.3 0-24 10.7-24 24s10.7 24 24 24h72 72c13.3 0 24-10.7 24-24s-10.7-24-24-24H216V430.4c85.8-11.7 152-85.3 152-174.4V216c0-13.3-10.7-24-24-24s-24 10.7-24 24v40c0 70.7-57.3 128-128 128s-128-57.3-128-128V216z"/></svg>`,
+              }}
+            ></div>
+          )
+        ) : null}
+      </div>
+    );
+  }
+
+  const [sex, setSex] = useState<string | null>(null);
+  const [age, setAge] = useState<string | null>(null);
 
   return (
     <main
@@ -142,49 +192,30 @@ function Home() {
       ) : null}
       <div className="w-full relative">
         <div className="w-full relative">
+          {listView ? (
+            <label htmlFor="SymptomsInput" className="text-sm">
+              Symptoms
+            </label>
+          ) : null}
           <textarea
-            onKeyDown={(e) => {
-              if (e.key === "Enter" && !e.shiftKey) {
-                e.preventDefault();
-                send();
-              }
+            // onKeyDown={(e) => {
+            //   if (e.key === "Enter" && !e.shiftKey) {
+            //     e.preventDefault();
+            //     send();
+            //   }
+            // }}
+            onChange={() => {
+              setListView(true);
             }}
             id="SymptomsInput"
             ref={textareaRef}
             placeholder="Enter your symptoms..."
-            className="flex h-20 w-full rounded-md border border-input bg-transparent px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+            className={textareaClassName.replace(
+              listView ? "" : " mt-2 ",
+              "  "
+            )}
           ></textarea>
-          <div className="absolute bottom-3 right-3 flex flex-row gap-x-4 items-center">
-            {recognition ? (
-              running ? (
-                <div
-                  className="w-3 h-3 absolute right-8 cursor-pointer bg-red-900 p-0 record-pulse box-content border-0"
-                  onClick={() => {
-                    recognition!.stop();
-                    setRunning(false);
-                  }}
-                ></div>
-              ) : (
-                <div
-                  className="w-3 cursor-pointer"
-                  onClick={() => {
-                    recognition!.start();
-                    setRunning(true);
-                  }}
-                  dangerouslySetInnerHTML={{
-                    __html: `<svg xmlns="http://www.w3.org/2000/svg" fill="white" viewBox="0 0 384 512"><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M192 0C139 0 96 43 96 96V256c0 53 43 96 96 96s96-43 96-96V96c0-53-43-96-96-96zM64 216c0-13.3-10.7-24-24-24s-24 10.7-24 24v40c0 89.1 66.2 162.7 152 174.4V464H120c-13.3 0-24 10.7-24 24s10.7 24 24 24h72 72c13.3 0 24-10.7 24-24s-10.7-24-24-24H216V430.4c85.8-11.7 152-85.3 152-174.4V216c0-13.3-10.7-24-24-24s-24 10.7-24 24v40c0 70.7-57.3 128-128 128s-128-57.3-128-128V216z"/></svg>`,
-                  }}
-                ></div>
-              )
-            ) : null}
-            <div
-              className="w-4 cursor-pointer"
-              onClick={send}
-              dangerouslySetInnerHTML={{
-                __html: `<svg xmlns="http://www.w3.org/2000/svg" fill="white" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M498.1 5.6c10.1 7 15.4 19.1 13.5 31.2l-64 416c-1.5 9.7-7.4 18.2-16 23s-18.9 5.4-28 1.6L284 427.7l-68.5 74.1c-8.9 9.7-22.9 12.9-35.2 8.1S160 493.2 160 480V396.4c0-4 1.5-7.8 4.2-10.7L331.8 202.8c5.8-6.3 5.6-16-.4-22s-15.7-6.4-22-.7L106 360.8 17.7 316.6C7.1 311.3 .3 300.7 0 288.9s5.9-22.8 16.1-28.7l448-256c10.7-6.1 23.9-5.5 34 1.4z"/></svg>`,
-              }}
-            ></div>
-          </div>
+          <Microphone id="SymptomsInput" />
         </div>
         {listView && data ? (
           <div className="mt-4 grid grid-cols-3 gap-4">
@@ -207,6 +238,77 @@ function Home() {
             })}
           </div>
         ) : null}
+
+        {listView && (
+          <>
+            <div className="w-full relative mt-4">
+              <label htmlFor="MedicalHistoryInput" className="text-sm">
+                Medical History
+              </label>
+              <textarea
+                id="MedicalHistoryInput"
+                placeholder="Enter your past medical history..."
+                className={textareaClassName}
+              ></textarea>
+              <Microphone id="MedicalHistoryInput" />
+            </div>
+            <div className="w-full relative mt-4">
+              <label htmlFor="MedicationsInput" className="text-sm">
+                Medications/Lifestyle Factors
+              </label>
+              <textarea
+                id="MedicationsInput"
+                placeholder="Enter your medications and lifestyle factors..."
+                className={textareaClassName}
+              ></textarea>
+              <Microphone id="MedicationsInput" />
+            </div>
+            <div className="mt-4">
+              <label className="text-sm mb-2 block">Sex</label>
+              <Select onValueChange={(value) => setSex(value)}>
+                <SelectTrigger className="w-full md:w-[200px]">
+                  <SelectValue placeholder="Sex" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="male">Male</SelectItem>
+                  <SelectItem value="female">Female</SelectItem>
+                  <SelectItem value="intersex">Intersex</SelectItem>
+                  <SelectItem value="other">Other</SelectItem>
+                  <SelectItem value="private">Prefer not to say</SelectItem>
+                </SelectContent>
+              </Select>
+            </div>
+            <div className="mt-4">
+              <label className="text-sm mb-2 block">Age</label>
+              <Select onValueChange={(value) => setAge(value)}>
+                <SelectTrigger className="w-full md:w-[200px]">
+                  <SelectValue placeholder="Age" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    {[...Array(100 - 18 + 1).fill(0)]
+                      .map((_, index) => index + 18)
+                      .map((age) => {
+                        return (
+                          <SelectItem value={age.toString()} key={age}>
+                            {age}
+                          </SelectItem>
+                        );
+                      })}
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+            </div>
+            <div className="mt-4">
+              <Button onClick={() => void send()} className="w-full sm:w-max">
+                {isFetching && (
+                  <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                )}
+                Calculate Possibilities
+              </Button>
+            </div>
+          </>
+        )}
       </div>
     </main>
   );
